d428237d389c265a55e279e5eb12a8172c363424
Zesty <zethsatentes@yahoo.com>
2022-04-05 22:13:43 -0700

    Tried to get the quiz working but didn't have enough time

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
+
 const questionContainerElement = document.getElementById("question-class")
 const questionElement = document.getElementById("questions")
 const answerButtonsElement = document .getElementById('answer-buttons')
@@ -9,7 +11,6 @@ startButton.addEventListener("click", startGame)
 
 
 function startGame() { 
-    console.log("Started")
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -40,16 +41,35 @@ function showQuestion(question) {
 
 }
 
-// function resetState() {
-//     nextButton.classList.add('hide')
-//     while (answerButtonsElement.firstChild) {
-//         answerButtonsElement.removeChild
-//         (answerButtonsElement.firstChild)
-//     }
-// }
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
 
-function selectAnswer () { 
+function selectAnswer (e) { 
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+}   else {
+    element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
 }
 
 const questions = [
